44a09696f793fef2bdd4f693f01523a408506dac
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2024-01-21 10:20:26 +0100

    Javadoc sur le controller stagiaire

--- filrouge_spring_2.0/src/main/java/com/fr/cch/filrouge/controller/StagiaireController.java
+++ filrouge_spring_2.0/src/main/java/com/fr/cch/filrouge/controller/StagiaireController.java
@@ -8,17 +8,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller pour un stagiaire
+ */
 @RestController
 @RequestMapping("/stagiaires")
 public class StagiaireController {
 
+    /**
+     * Appelle le service Stagiaires
+     */
     public final StagiaireService stagiaireService;
 
+    /**
+     * Constructeur
+     * @param stagiaireService le service Stagiaires
+     */
     @Autowired
     public StagiaireController(StagiaireService stagiaireService) {
         this.stagiaireService = stagiaireService;
     }
 
+    /**
+     * Méthode pour créer un stagiaire
+     * @param stagiaire le stagiaire à créer
+     * @return le stagiaire nouvellement créé
+     */
     @PostMapping("/create")
     public Stagiaire createStagiaire(@RequestBody Stagiaire stagiaire) {
         return stagiaireService.createStagiaire(stagiaire);
